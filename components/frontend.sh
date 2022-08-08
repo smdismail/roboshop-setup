1e37a7fb669c638cf40a02695acae8323ab173db
Ismail <ismail.smd1981@gmail.com>
2022-08-08 22:16:25 +0400

    sudo user

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 #Running Roboshop Frontend with Sudo permission
+
+USER_ID=$(id -u)
+
+if [ "$USER_ID" -ne 0 ]
+then
+  echo Your suppose to be run this as a Root or Sudo user
+else
+
 yum install nginx -y
 systemctl enable nginx
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
@@ -9,3 +17,5 @@ unzip /tmp/frontend.zip
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
+
+fi

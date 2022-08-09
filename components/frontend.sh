099408d2b2e9b9d96efd89c7007a6269f6c382da
Ismail <ismail.smd1981@gmail.com>
2022-08-09 18:51:57 +0400

    logs

--- components/frontend.sh
+++ components/frontend.sh
@@ -5,13 +5,30 @@ source components/common.sh
 
 checkRootUser
 
+echo "installing nginx"
 yum install nginx -y >/tmp/roboshop.log
-systemctl enable nginx >/tmp/roboshop.log
+
+
+echo "downloading frontend code"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >/tmp/roboshop.log
+
 cd /usr/share/nginx/html
+
+echo "removing old files"
 rm -rf * >/tmp/roboshop.log
+
+echo "extracting zip content"
 unzip /tmp/frontend.zip >/tmp/roboshop.log
-mv frontend-main/static/* . >/tmp/roboshop.log
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf >/tmp/roboshop.log
+
+echo "copying extracted content"
+mv frontend-main/* . >/tmp/roboshop.log
+mv static/* . >/tmp/roboshop.log
+rm -rf frontend-main README.md >/tmp/roboshop.log
+
+echo "copying extracted content"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf >/tmp/roboshop.log
+
+echo "Start nginx service"
+systemctl enable nginx >/tmp/roboshop.log
 systemctl restart nginx >/tmp/roboshop.log
 

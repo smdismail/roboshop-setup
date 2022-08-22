de761c100afdbfc8b6d817cfa1b66aa15a68a2f1
Ismail <ismail.smd1981@gmail.com>
2022-08-22 20:23:31 +0400

    fe new chnges

--- components/frontend.sh
+++ components/frontend.sh
@@ -23,19 +23,21 @@ unzip /tmp/frontend.zip &>>${LOG_FILE}
 statusCheck $?
 
 ECHO "Copying extracted Content"
-mv frontend-main/* . &>>${LOG_FILE} && mv static/* . &>>${LOG_FILE} && rm -rf frontend-main README.md &>>${LOG_FILE}
+mv frontend-main/* . &>>${LOG_FILE}
+mv static/* . &>>${LOG_FILE}
+rm -rf frontend-main README.md &>>${LOG_FILE}
 statusCheck $?
 
 ECHO "Copy RoboShop Nginx Config"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>${LOG_FILE}
 statusCheck $?
 
-ECHO "Update Nginx Configuration"
-for component in catalogue user cart shipping payment ; do
-  ECHO "Update Configuration for ${component}"
-  sed -i -e "/${component}/ s/localhost/${component}.roboshop.internal/"  /etc/nginx/default.d/roboshop.conf
-  statusCheck $?
-done
+#ECHO "Update Nginx Configuration"
+#for component in catalogue user cart shipping payment ; do
+#  ECHO "Update Configuration for ${component}"
+#  sed -i -e "/${component}/ s/localhost/${component}.roboshop.internal/"  /etc/nginx/default.d/roboshop.conf
+#  statusCheck $?
+#done
 
 ECHO "Start Nginx Service"
 systemctl enable nginx &>>${LOG_FILE} && systemctl restart nginx &>>${LOG_FILE}

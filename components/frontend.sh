a1f8e02298a41482e6d964173f0d060c59796220
Ismail <ismail.smd1981@gmail.com>
2022-08-22 20:14:02 +0400

    fe new chnges

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env bash
-#Running Roboshop Frontend with Sudo permission
 
 source components/common.sh
-#sample running frontend
 checkRootUser
 
-ECHO "installing nginx"
-yum install nginx -y >>${LOG_FILE}
+
+ECHO "Installing Nginx"
+yum install nginx -y &>>${LOG_FILE}
 statusCheck $?
 
-ECHO "downloading frontend code"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>${LOG_FILE}
+ECHO "Downloading Frontend Code"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>${LOG_FILE}
 statusCheck $?
+
 cd /usr/share/nginx/html
 
-ECHO "removing old files"
-rm -rf * >>${LOG_FILE}
+ECHO "Removing Old Files"
+rm -rf * &>>${LOG_FILE}
 statusCheck $?
 
-ECHO "extracting zip content"
-unzip /tmp/frontend.zip >>${LOG_FILE}
+ECHO "Extracting Zip Content"
+unzip /tmp/frontend.zip &>>${LOG_FILE}
 statusCheck $?
 
-ECHO "copying extracted content"
-mv frontend-main/* . >>${LOG_FILE} && mv static/* . >>${LOG_FILE} && rm -rf frontend-main README.md >>${LOG_FILE}
+ECHO "Copying extracted Content"
+mv frontend-main/* . &>>${LOG_FILE} && mv static/* . &>>${LOG_FILE} && rm -rf frontend-main README.md &>>${LOG_FILE}
 statusCheck $?
 
-ECHO "copying robshop nginx config"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf >>${LOG_FILE}
+ECHO "Copy RoboShop Nginx Config"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>${LOG_FILE}
 statusCheck $?
 
 ECHO "Update Nginx Configuration"
@@ -37,7 +37,6 @@ for component in catalogue user cart shipping payment ; do
   statusCheck $?
 done
 
-ECHO "Start nginx service"
-systemctl enable nginx >>${LOG_FILE} && systemctl restart nginx >>${LOG_FILE}
+ECHO "Start Nginx Service"
+systemctl enable nginx &>>${LOG_FILE} && systemctl restart nginx &>>${LOG_FILE}
 statusCheck $?
-#echo test print
